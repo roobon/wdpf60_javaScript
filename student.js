e5574cc5a60a45663574c81ca1dab72f39cb6639
SaifulAlam93 <gmsifulalam93@gmail.com>
2024-06-05 13:09:05 +0600

    05 June Classwork

---
+++ student.js
@@ -0,0 +1,11 @@
+const student_info = () => {
+    let name = "Rahim";
+    let address = "Mirpur";
+    return `Name:  ${name} Address: ${address}`;
+}
+
+let country =  "Bangladesh";
+let district = "Dhaka";
+
+export default student_info;
+export {district, country};
